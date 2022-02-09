964fad0fe44dd8941a918b5dfccd866c55da45d0
jeandrei <jeandreiwalter@gmail.com>
2022-02-09 18:50:47 +0000

    Ate o 07

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
+//Constantes de configuração que vem do arquivo dotenv
 require('dotenv').config();
 
-console.log('Configuração do arquivo .env');
+//Definição da porta onde vai rodar o app
+const port = process.env.PORT || 3000;
 
-const secreto = process.env.SECRETO;
+//Require do express
+const express = require('express');
+const app = express();
 
-console.log (`O valor passado pelo dotenv é ${secreto}`);
+//Possibilitar acesso ao req.body
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+//Const com o path do app
+const path = require('path');
+
+//Definição do diretório onde iremos colocar os formulários e views
+app.set('views', path.join(__dirname, 'views'));
+
+//Definimos o framework usado ejs
+app.set('view engine', 'ejs');
+
+//===============================================================
+//Carrega o arquivo /views/cadastro/new.ejs onde está o formulário
+app.get('/cadastros/new', (req, res) => {
+    res.render('cadastros/new');
+});
+//O formulário está enviando os dados para a rota /cadastros que é esta aqui
+//onde temos acesso aos dados enviados
+//para app.post depende necessariamente da linha app.use(express.urlencoded({ extended: true }));
+app.post('/cadastros', (req, res) => {
+    //depende do urlencoded para visualizar os dados do req.body
+    console.log(req.body);
+});
+//===============================================================
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+});
